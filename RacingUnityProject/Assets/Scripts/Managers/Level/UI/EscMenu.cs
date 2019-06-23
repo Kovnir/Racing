d4741345aa2c38a4268c2bdbadabcd393bd122cf
Albert <akovnir@playq.net>
2019-06-23 13:17:44 +0300

    Updated Quit feature

--- RacingUnityProject/Assets/Scripts/Managers/Level/UI/EscMenu.cs
+++ RacingUnityProject/Assets/Scripts/Managers/Level/UI/EscMenu.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
+using JetBrains.Annotations;
+using UnityEditor;
 using UnityEngine;
 using Zenject;
 
@@ -8,17 +10,21 @@ public class EscMenu : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
     [InjectOptional] private LoaderViewManager loaderViewManager;
+    
+    [UsedImplicitly]
     public void OnResumeClick()
     {
         SetPause(false);
     }
     
+    [UsedImplicitly]
     public void OnRestartClick()
     {
         Time.timeScale = 1;
         loaderViewManager.ReloadLevel();        
     }
     
+    [UsedImplicitly]
     public void OnMainMenuClick()
     {
         Time.timeScale = 1;
@@ -32,7 +38,11 @@ public class EscMenu : MonoBehaviour
     
     public void OnQuitClick()
     {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 
     private bool pauseMode;
