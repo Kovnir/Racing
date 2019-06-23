f20ff2dc5a6ba2da17b5b6f19e0d6323390b4f59
Albert <akovnir@playq.net>
2019-06-23 12:55:36 +0300

    Added base for main menu

---
+++ RacingUnityProject/Assets/Scripts/Managers/Level/UI/EscMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    
+    public void OnResumeClick()
+    {
+        SetPause(false);
+    }
+    
+    public void OnRestartClick()
+    {
+        
+    }
+    
+    public void OnMainMenuClick()
+    {
+        
+    }
+    
+    public void OnPostProcessingClick()
+    {
+        
+    }
+    
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+
+    private bool pauseMode;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMode = !pauseMode;
+            SetPause(pauseMode);
+        }
+    }
+
+    private void SetPause(bool isPause)
+    {
+        Time.timeScale = isPause ? 0 :1;
+        panel.SetActive(isPause);
+    }
+}
